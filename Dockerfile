0119a2815492caf2b4ce2d44a05ccdecbaaeddf5
Alessio <alessio.attanasi@ciselab.org>
2018-04-20 16:35:51 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:8
+
+RUN wget -q https://services.gradle.org/distributions/gradle-4.6-bin.zip \
+    && unzip gradle-4.6-bin.zip -d /opt \
+    && rm gradle-4.6-bin.zip
+
+ENV GRADLE_HOME /opt/gradle-4.6
+ENV PATH $PATH:/opt/gradle-4.6/bin
---
+++ src/test/java/TestCase1.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class TestCase1 {
+	
+    @Test
+	public void test() {
+        
+		Client c = new Client(4,10,'+');
+    
+	}
+
+}
